84474809203ec8f0eae4b6f6f846efbb267af11c
Dimych <kuzyuberdin@gmail.com>
2019-08-02 19:42:06 +0300

    put data from state to component

--- src/DragonChiken/components/DragonChickenBurger.js
+++ src/DragonChiken/components/DragonChickenBurger.js
@@ -1,6 +1,6 @@
 import React from "react";
 import GoogleMapReact from 'google-map-react';
-import {Provider} from "react-redux";
+import {connect, Provider} from "react-redux";
 import store from "../redux/store";
 
 const Unit = (props) => <div>
@@ -11,7 +11,7 @@ const Unit = (props) => <div>
 let DragonChickenBurgerView = ({center = {
     lat: 59.95,
     lng: 30.33
-}, zoom = 11}) => {
+}, zoom = 11, units = []}) => {
     return (
             <div style={{ height: '100vh', width: '100%' }}>
                 <GoogleMapReact
@@ -19,24 +19,24 @@ let DragonChickenBurgerView = ({center = {
                     defaultCenter={center}
                     defaultZoom={zoom}
                 >
+                    {units.map(u =>   <Unit
+                        lat={u.coords.lat}
+                        lng={u.coords.lng}
+                        src={u.src}
+                    />)}
 
-                    <Unit
-                        lat={59.905415}
-                        lng={30.337844}
-                        text="hey"
-                        src={"https://www.pinclipart.com/picdir/middle/59-598395_chicken-clipart-png-download.png"}
-                    />
-                    <Unit
-                        lat={59.965415}
-                        lng={30.337844}
-                        text="hey"
-                        src={"https://art.pixilart.com/b163c360b05cfd8.gif"}
-                    />
+
                 </GoogleMapReact>
             </div>
 
     )
 }
+
+let mstp = (state) => ({
+    units: state.dragonChicken.units
+})
+DragonChickenBurgerView = connect(mstp)(DragonChickenBurgerView);
+
 
 let DragonChickenBurger = (props) => <Provider store={store}>
     <DragonChickenBurgerView {...props}/>
--- src/DragonChiken/redux/reducer.js
+++ src/DragonChiken/redux/reducer.js
@@ -5,10 +5,10 @@ const initialState = {
         {
             id: 1,
             name: "Chicken",
+            src: "https://www.pinclipart.com/picdir/middle/59-598395_chicken-clipart-png-download.png",
             coords: {
                 lat: 59.965415,
-                lng: 30.337844,
-                src: "https://www.pinclipart.com/picdir/middle/59-598395_chicken-clipart-png-download.png"
+                lng: 30.337844
             }
         },
         {
@@ -16,9 +16,9 @@ const initialState = {
             name: "Dragon",
             coords: {
                 lat: 59.995415,
-                lng: 30.337844,
-                src: "https://art.pixilart.com/b163c360b05cfd8.gif"
-            }
+                lng: 30.337844
+            },
+            src: "https://art.pixilart.com/b163c360b05cfd8.gif"
         }
     ]
 
