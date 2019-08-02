453c2ca674bcb66ef6cd580ab7f967e94b2025f5
Dimych <kuzyuberdin@gmail.com>
2019-08-02 21:10:46 +0300

    2 units are movving with bug

--- src/DragonChiken/components/DragonChickenBurger.js
+++ src/DragonChiken/components/DragonChickenBurger.js
@@ -2,7 +2,7 @@ import React from "react";
 import GoogleMapReact from 'google-map-react';
 import {connect, Provider} from "react-redux";
 import store from "../redux/store";
-import {DIRECTIONS, move} from "../redux/reducer";
+import {DIRECTIONS, move, stop} from "../redux/reducer";
 
 const Unit = (props) => <div>
     <img style={{width: "20px", height: "20px"}}
@@ -13,11 +13,12 @@ let DragonChickenBurgerView = ({
                                    center = {
                                        lat: 59.95,
                                        lng: 30.33
-                                   }, zoom = 11, units = [], move
+                                   }, zoom = 11, units = [],
+                                   move, stop
                                }) => {
 
     const onKeyDown = (e) => {
-       console.log("sdsdsd");
+       console.log(e.key);
 
         switch (e.key) {
             case "w":
@@ -33,10 +34,50 @@ let DragonChickenBurgerView = ({
                 move(DIRECTIONS.LEFT, 1);
                 break;
         }
+        switch (e.key) {
+            case "i":
+                move(DIRECTIONS.UP, 2);
+                break;
+            case "j":
+                move(DIRECTIONS.DOWN, 2);
+                break;
+            case "l":
+                move(DIRECTIONS.RIGHT, 2);
+                break;
+            case "k":
+                move(DIRECTIONS.LEFT, 2);
+                break;
+        }
     }
     const onKeyUp = (e) => {
-
-
+        switch (e.key) {
+            case "w":
+                stop(DIRECTIONS.UP, 1);
+                break;
+            case "s":
+                stop(DIRECTIONS.DOWN, 1);
+                break;
+            case "d":
+                stop(DIRECTIONS.RIGHT, 1);
+                break;
+            case "a":
+                stop(DIRECTIONS.LEFT, 1);
+                break;
+        }
+        switch (e.key) {
+            case "i":
+                stop(DIRECTIONS.UP, 2);
+                break;
+            case "j":
+                stop(DIRECTIONS.DOWN, 2);
+                break;
+            case "l":
+                stop(DIRECTIONS.RIGHT, 2);
+                break;
+            case "k":
+                stop(DIRECTIONS.LEFT, 2);
+                break;
+        }
     }
 
     return (
@@ -64,7 +105,7 @@ let DragonChickenBurgerView = ({
 let mstp = (state) => ({
     units: state.dragonChicken.units
 })
-DragonChickenBurgerView = connect(mstp, {move})(DragonChickenBurgerView);
+DragonChickenBurgerView = connect(mstp, {move, stop})(DragonChickenBurgerView);
 
 
 let DragonChickenBurger = (props) => <Provider store={store}>
--- src/DragonChiken/redux/reducer.js
+++ src/DragonChiken/redux/reducer.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 const MOVE_UNIT_BY_STEP = "MOVE_UNIT_BY_STEP";
+const STOP_UNIT = "STOP_UNIT";
 
 export const DIRECTIONS = {
     UP: "UP",
@@ -18,7 +19,8 @@ const initialState = {
             coords: {
                 lat: 59.965415,
                 lng: 30.337844
-            }
+            },
+            moveDirection: null
         },
         {
             id: 2,
@@ -27,7 +29,8 @@ const initialState = {
                 lat: 59.995415,
                 lng: 30.337844
             },
-            src: "https://art.pixilart.com/b163c360b05cfd8.gif"
+            src: "https://art.pixilart.com/b163c360b05cfd8.gif",
+            moveDirection: null
         }
     ]
 
@@ -44,7 +47,8 @@ const getNewCoords = (direction, coords) => {
             return {...coords, lng: coords.lng + STEP}
         case DIRECTIONS.LEFT:
             return {...coords, lng: coords.lng - STEP}
-        default: return coords;
+        default:
+            return coords;
     }
 }
 
@@ -52,12 +56,25 @@ const reducer = (state = initialState, action) => {
 
     switch (action.type) {
         case MOVE_UNIT_BY_STEP:
-            return {...state,
-                units: state.units.map( u => {
+            return {
+                ...state,
+                units: state.units.map(u => {
                     if (u.id != action.unitId) return u;
-                    return { ...u, coords: getNewCoords(action.direction, u.coords)}
+                    return {
+                        ...u,
+                        moveDirection: action.direction,
+                        coords: getNewCoords(action.direction, u.coords)
+                    }
                 })
             }
+        case STOP_UNIT:
+            state.units.map(u => {
+                if (u.id != action.unitId) return u;
+                return {
+                    ...u,
+                    moveDirection: null
+                }
+            })
 
         default:
             return state;
@@ -67,14 +84,39 @@ const reducer = (state = initialState, action) => {
 }
 
 const moveUnitByStep = (direction, unitId) => ({type: MOVE_UNIT_BY_STEP, direction, unitId});
+const stopUnit = (unitId) => ({type: STOP_UNIT, unitId});
 
 const STEP = 0.0001;
 
-export const move = (direction, unitId) => (dispatch, getState) => {
-    setInterval(() => {
-        dispatch(moveUnitByStep(direction, unitId));
+let intervals = {
+    /*"1-Y": 3,
+    "1-X": 4,
+    "2-Y": 5*/
+};
 
+const XYDetection = (direction) =>{
+    if (direction == DIRECTIONS.UP || direction == DIRECTIONS.DOWN) return "X";
+    if (direction == DIRECTIONS.RIGHT || direction == DIRECTIONS.LEFT) return "Y";
+}
+
+export const move = (direction, unitId) => (dispatch, getState) => {
+    if (getState().dragonChicken.units.find(u => u.id == unitId).moveDirection == direction) {
+        return;
+    }
+    let intervalID = unitId+ "-" + XYDetection(direction);
+    clearInterval(intervals[intervalID]);
+
+    intervals[intervalID] = setInterval(() => {
+
+        dispatch(moveUnitByStep(direction, unitId));
     }, 100);
+}
+
+export const stop = (direction, unitId) => (dispatch) => {
+
+    let intervalID = unitId+ "-" + XYDetection(direction);
+    clearInterval(intervals[intervalID]);
+    dispatch(stopUnit(unitId));
 }
 
 
